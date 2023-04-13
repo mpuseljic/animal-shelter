09f29a17f3eb2155780a06670c652b899acc5762
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-04-13 17:52:34 +0200

    Create RegisterView.vue

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="first"
+            label="First Name"
+            filled
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-text-field
+            v-model="last"
+            label="Last Name"
+            filled
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+
+
+<script>
+ 
+  export default {
+    name: "RegisterView",
+    data(){
+      return {
+        //properties
+      }
+    }
+  }
+</script>
